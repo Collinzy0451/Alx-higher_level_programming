dd0a36f90058c9955478c49efbc5c436860c0c3a
collinzy0451 <tamunosakicollinsjacob@gmail.com>
2024-05-22 00:29:57 +0000

    statuscode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,22 @@
-#!/usr/bin/node
-const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], error => {
-  if (error) console.log(error);
+#!/usr/bin/env node
+
+const request = require('request');
+
+// Get the URL from the command line arguments
+const url = process.argv[2];
+
+if (!url) {
+    console.error('Usage: ./2-statuscode.js <URL>');
+    process.exit(1);
+}
+
+// Make a GET request to the provided URL
+request(url, (error, response) => {
+    if (error) {
+        console.error('Error:', error);
+        process.exit(1);
+    }
+    // Print the status code
+    console.log(`code: ${response.statusCode}`);
 });
+
